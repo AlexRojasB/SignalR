e0b3628ce9be5900e33287ba7ba08a688771fc9d
= <=>
2024-07-10 23:22:17 -0600

    strongly typed hubs

--- server/Hubs/ViewHub.cs
+++ server/Hubs/ViewHub.cs
@@ -1,17 +1,21 @@
 using Microsoft.AspNetCore.SignalR;
 
 namespace server.Hubs;
-public class ViewHub : Hub 
+public class ViewHub : Hub<IHubClient> 
 {
     public static int ViewCount {get;set;} = 0;
 
     public async override Task OnConnectedAsync() {
         ViewCount++;
-        await Clients.All.SendAsync("viewCountUpdate", ViewCount);
+        await Clients.All.ViewCountUpdate(ViewCount);
     }
 
     public async override Task OnDisconnectedAsync(Exception? exception) {
         ViewCount--;
-        await Clients.All.SendAsync("viewCountUpdate", ViewCount);
+        await Clients.All.ViewCountUpdate(ViewCount);
     }
+}
+
+public interface IHubClient {
+    Task ViewCountUpdate(int viewCount);
 }
